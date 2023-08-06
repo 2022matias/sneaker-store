5b1a0a98661a3f20777eb2da4d8d65cc7425263b
Matias Campos <lolo252000@gmail.com>
2023-08-05 23:39:21 -0300

    iniciando proyecto

---
+++ my-react-app/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { CurrentUserContext } from './contexts/currentUserContext';
+import Main from './components/Main';
+import { NavLink } from 'react-router-dom';
+import Header from './components/Header';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="page">
+      <CurrentUserContext.Provider>
+        <Header />
+        <NavBar />
+        {/* <NavLink to="/man">Hombre</NavLink>
+          <NavLink to="/girl">Mujer</NavLink>
+          <NavLink to="/company">Empresa</NavLink>
+          <NavLink to="/contact">contacto</NavLink> */}
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+        <Footer />
+      </CurrentUserContext.Provider>
+    </div>
+
+  );
+
+}
+export default App;
---
+++ my-react-app/src/contexts/currentUserContext.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const CurrentUserContext = React.createContext();
+
+export default function CurrentUserProvider({ children }) {
+    const [currentUser, setCurrentUser] = React.useState(null);
+    const [counter, setCounter] = React.useState(0);
+    return (
+        <CurrentUserContext.Provider value={[currentUser, setCurrentUser, counter, setCounter]}>
+            {children}
+        </CurrentUserContext.Provider>
+    );
+}
